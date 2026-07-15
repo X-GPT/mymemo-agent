c410ffc2eed6adb1bc3ae621e83ac9d15ed053d4
chengchao <chengchao60827@gmail.com>
2026-07-15 19:39:55 +0800

    fix(infra): allow deploy role to inspect instance profiles (#291)

--- infra/bootstrap-iam/main.tf
+++ infra/bootstrap-iam/main.tf
@@ -213,6 +213,7 @@ data "aws_iam_policy_document" "github_actions_deploy" {
       "iam:GetRole",
       "iam:GetRolePolicy",
       "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
       "iam:ListPolicyVersions",
       "iam:ListRolePolicies",
       "iam:PassRole",
